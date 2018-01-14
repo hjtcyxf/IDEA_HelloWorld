540aee143f40304d4cc3545db8ab7644f7c49d9c
hjtcyxf <hjtcyxf@163.com>
2018-01-14 11:45:20 +0800

    HelloWorld

--- com/study/demo/HelloWorld.java
+++ com/study/demo/HelloWorld.java
@@ -1,9 +1,7 @@
-public class Main {
+package com.study.demo;
 
+public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        if (1 == 1) {
-
-        }
     }
 }
